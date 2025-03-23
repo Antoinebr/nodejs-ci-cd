23fcfe7e3c7ccbff28fa25e50ce763f58bb91cda
Antoine  Brossault <antoinebrossault@gmail.com>
2025-03-23 14:42:39 +0100

    up

--- test/integration.test.js
+++ test/integration.test.js
@@ -15,4 +15,19 @@ describe("Integration Tests", () => {
     expect(res.status).to.equal(200);
     expect(res.body).to.deep.equal({ message: "API is working" });
   });
+
+  it("GET /api should return JSON response", async () => {
+    const res = await request(app).get("/api");
+    expect(res.status).to.equal(200);
+    expect(res.body).to.deep.equal({ message: "API is working" });
+  });
+
+  // test the /403 route it should return a 403 with a JSON response
+  it("GET /403 should return 403 with JSON response", async () => {
+    const res = await request(app).get("/api");
+    expect(res.status).to.equal(403);
+    expect(res.body).to.deep.equal({ message: "API is working" });
+  });
+
+
 });
